ce53ee1b1aa52d8317b71eace3228f4cd7ad2dfb
novinq <164775877+NovexTheRizzler@users.noreply.github.com>
2024-07-04 21:30:39 +0500

    Init.py Fr

---
+++ Helper/__Init__.py
@@ -0,0 +1,37 @@
+__title__ = 'Quorix Fake DDoS'
+__author__ = 'Novex / Novinq'
+__copyright__ = 'discord.gg/subscribers'
+__version__ = '1.0.0'
+
+import time
+import random
+import colorama
+import pystyle
+import ab5
+import ctypes
+import os
+
+from colorama import Fore, Style
+from ab5 import vgratient
+from pystyle import *
+
+#####################
+L1 = "\x1b[38;5;177m"
+L = "\x1b[38;5;97m"
+
+hash = f"{L}[{Fore.WHITE}#{L}] {L1}"
+plus = f"{L}[{Fore.WHITE}+{L}] {L1}"
+mark = f"{L}[{Fore.WHITE}x{L}] {L1}"
+minus = f"{L}[{Fore.WHITE}-{L}] {L1}"
+bannerc = "\x1b[38;5;135m"
+
+
+banner1 = f"""                  
+                                 {bannerc} ██████╗ ██╗   ██╗ ██████╗ ██████╗ ██╗██╗  ██╗ 
+                                 ██╔═══██╗██║   ██║██╔═══██╗██╔══██╗██║╚██╗██╔╝ {L1} > Fake DDoS
+                                 ██║   ██║██║   ██║██║   ██║██████╔╝██║ ╚███╔╝  {L1} > Made By Novex
+                                 ██║▄▄ ██║██║   ██║██║   ██║██╔══██╗██║ ██╔██╗  {L1} > .gg/subscribers
+                                 ╚██████╔╝╚██████╔╝╚██████╔╝██║  ██║██║██╔╝ ██╗
+                                  ╚══▀▀═╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ 
+                                  """  
+####################
